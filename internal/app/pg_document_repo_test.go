09de28d8773e72fe14ab6aa8fd1b6695085c40ee
tsatke <tim.satke@gmail.com>
2021-06-03 13:02:02 +0200

    failure tests for prepare statements in docs.Create

--- internal/app/pg_document_repo_test.go
+++ internal/app/pg_document_repo_test.go
@@ -76,6 +76,37 @@ func (suite *PostgresDocumentRepoTestSuite) TestCreate() {
 	}))
 }
 
+func (suite *PostgresDocumentRepoTestSuite) TestCreateFailHeaderPrepare() {
+	docCreateTime := time.Now()
+
+	testErr := errors.New("test error")
+	suite.mock.
+		ExpectBegin()
+	suite.mock.
+		ExpectPrepare(`INSERT INTO au_document_headers (doc_id, name, size, owner, created) VALUES ($1, $2, $3, $4, $5)`).
+		WillReturnError(testErr)
+	suite.mock.
+		ExpectRollback()
+
+	suite.ErrorIs(suite.index.Create(DocumentHeader{
+		ID:      "docID",
+		Name:    "docName",
+		Size:    1234,
+		Owner:   "username",
+		Created: docCreateTime,
+	}, ACL{
+		Permissions: map[string]Permission{
+			"username": {
+				Username: "username",
+				Read:     true,
+				Write:    true,
+				Delete:   true,
+				Share:    true,
+			},
+		},
+	}), testErr)
+}
+
 func (suite *PostgresDocumentRepoTestSuite) TestCreateFailHeader() {
 	docCreateTime := time.Now()
 
@@ -88,6 +119,44 @@ func (suite *PostgresDocumentRepoTestSuite) TestCreateFailHeader() {
 	prepHeader.
 		ExpectExec().
 		WithArgs("docID", "docName", 1234, "username", docCreateTime).
+		WillReturnError(testErr)
+	suite.mock.
+		ExpectRollback()
+
+	suite.ErrorIs(suite.index.Create(DocumentHeader{
+		ID:      "docID",
+		Name:    "docName",
+		Size:    1234,
+		Owner:   "username",
+		Created: docCreateTime,
+	}, ACL{
+		Permissions: map[string]Permission{
+			"username": {
+				Username: "username",
+				Read:     true,
+				Write:    true,
+				Delete:   true,
+				Share:    true,
+			},
+		},
+	}), testErr)
+}
+
+func (suite *PostgresDocumentRepoTestSuite) TestCreateFailACLPrepare() {
+	docCreateTime := time.Now()
+
+	testErr := errors.New("test error")
+	suite.mock.
+		ExpectBegin()
+	prepHeader := suite.mock.
+		ExpectPrepare(`INSERT INTO au_document_headers (doc_id, name, size, owner, created) VALUES ($1, $2, $3, $4, $5)`).
+		WillBeClosed()
+	prepHeader.
+		ExpectExec().
+		WithArgs("docID", "docName", 1234, "username", docCreateTime).
+		WillReturnResult(sqlmock.NewResult(0, 1))
+	suite.mock.
+		ExpectPrepare(`INSERT INTO au_document_acls (doc_id, username, read, write, delete, share) VALUES ($1, $2, $3, $4, $5, $6)`).
 		WillReturnError(testErr)
 	suite.mock.
 		ExpectRollback()
